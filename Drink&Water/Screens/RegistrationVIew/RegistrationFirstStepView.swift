e815389c685af3cb668159a2511f23274eb53931
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2024-03-03 04:30:13 +0300

    Make registration UI

---
+++ Drink&Water/Screens/RegistrationVIew/RegistrationFirstStepView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationFirstStepView.swift
+//  Drink&Water
+//
+//  Created by Виктория Серикова on 03.03.2024.
+//
+
+import SwiftUI
+
+struct RegistrationFirstStepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationFirstStepView()
+}
---
+++ Drink&Water/Screens/RegistrationVIew/RegistrationSecondStepView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationSecondStepView.swift
+//  Drink&Water
+//
+//  Created by Виктория Серикова on 03.03.2024.
+//
+
+import SwiftUI
+
+struct RegistrationSecondStepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationSecondStepView()
+}
